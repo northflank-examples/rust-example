96c21538662290b3405aed4d61aa9793a06e930e
Bartlomiej Baran <32595868+BBato@users.noreply.github.com>
2021-03-10 16:05:37 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-let mut app = tide::new();
-app.at("/").get(|_| async { Ok("Hello, world!") });
-app.listen("127.0.0.1:8080").await?;
+
+#![feature(async_await)]
+
+extern crate tide;
+
+use tide::App;
+use std::{env, net::SocketAddr};
+
+
+
+fn main() {
+    let mut app = App::new(());
+    let address = SocketAddr::from(([127, 0, 0, 1], get_server_port()));
+
+    app.at("/").get(async move |_| "hello world");
+    app.serve(address).expect("Start server");
+}
+
+fn get_server_port() -> u16 {
+    env::var("PORT")
+        .ok()
+        .and_then(|port| port.parse().ok())
+        .unwrap_or_else(|| 8186)
+}
+
