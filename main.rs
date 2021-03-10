99eb8379436f29749d23ec6112ebcc5376128a3d
Bartlomiej Baran <32595868+BBato@users.noreply.github.com>
2021-03-10 15:00:46 +0000

    Init

---
+++ main.rs
@@ -0,0 +1,25 @@
+
+#![feature(async_await)]
+
+extern crate tide;
+
+use tide::App;
+use std::{env, net::SocketAddr};
+
+
+
+fn main() {
+    let mut app = App::new(());
+    let address = SocketAddr::from(([127, 0, 0, 1], get_server_port()));
+
+    app.at("/").get(async move |_| "hello world");
+    app.serve(address).expect("Start server");
+}
+
+fn get_server_port() -> u16 {
+    env::var("PORT")
+        .ok()
+        .and_then(|port| port.parse().ok())
+        .unwrap_or_else(|| 8186)
+}
+
